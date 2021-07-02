525bc6775abd54611a00ff48bb6d3eac8dd97fd2
fen hrir <fenhrir@pop-os.localdomain>
2021-07-02 15:36:48 +0200

    [*] corr of ip

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
+    port: process.env.MYSQL_PORT,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
